2e46e8af8b58b3d4f58f1a974a5a48cae98873d0
Dilushan <gunalandilushan@gmail.com>
2023-11-26 22:03:29 +0530

    updated app,logo

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import base64,random
 import time,datetime 
+
 #libraries to parse the resume pdf files
 from pyresparser import ResumeParser
 from pdfminer3.layout import LAParams, LTTextBox
@@ -13,70 +14,12 @@ import io,random
 from streamlit_tags import st_tags
 from PIL import Image
 import pymysql
-from courses import ds_course,web_course,android_course,ios_course,uiux_course,resume_videos,interview_videos
+from courses import ds_course,web_course,android_course,ios_course,uiux_course
+
 # import pafy #for uploading youtube videos
 import plotly.express as px #to create visualisations at the admin session
 import nltk
 #nltk.download('stopwords')
-
-# Diluxshana
-
-
-def fetch_yt_video(link):
-    video = pafy.new(link)
-    return video.title
-
-def get_table_download_link(df,filename,text):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    # href = f'<a href="data:file/csv;base64,{b64}">Download Report</a>'
-    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}">{text}</a>'
-    return href
-
-def pdf_reader(file):
-    resource_manager = PDFResourceManager()
-    fake_file_handle = io.StringIO()
-    converter = TextConverter(resource_manager, fake_file_handle, laparams=LAParams())
-    page_interpreter = PDFPageInterpreter(resource_manager, converter)
-    with open(file, 'rb') as fh:
-        for page in PDFPage.get_pages(fh,
-                                      caching=True,
-                                      check_extractable=True):
-            page_interpreter.process_page(page)
-            print(page)
-        text = fake_file_handle.getvalue()
-
-    # close open handles
-    converter.close()
-    fake_file_handle.close()
-    return text
-
-def show_pdf(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    # pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-def course_recommender(course_list):
-    st.subheader("**Courses & Certificates Recommendations 🎓**")
-    c = 0
-    rec_course = []
-    no_of_reco = st.slider('Choose Number of Course Recommendations:', 1, 10, 5)
-    random.shuffle(course_list)
-    for c_name, c_link in course_list:
-        c += 1
-        st.markdown(f"({c}) [{c_name}]({c_link})")
-        rec_course.append(c_name)
-        if c == no_of_reco:
-            break
-    return rec_course
-
-
 
 
 #CONNECT TO DATABASE
@@ -90,7 +33,10 @@ def insert_data(name,email,res_score,timestamp,no_of_pages,reco_field,cand_level
     rec_values = (name, email, str(res_score), timestamp,str(no_of_pages), reco_field, cand_level, skills,recommended_skills,courses)
     cursor.execute(insert_sql, rec_values)
 
-
+st.set_page_config(
+   page_title="AI Resume Analyzer",
+   page_icon='./Logo/logo2.png',
+)
 
 # Create the DB
 db_sql = """CREATE DATABASE IF NOT EXISTS resume;"""
@@ -115,3 +61,50 @@ table_sql = "CREATE TABLE IF NOT EXISTS " + DB_table_name + """
 cursor.execute(table_sql)
 
 
+def run():
+    img = Image.open('./Logo/logo2.png')
+    st.image(img)
+    st.title("AI Resume Analyser")
+    st.sidebar.markdown("# Choose User")
+    activities = ["User", "Admin"]
+    choice = st.sidebar.selectbox("Choose among the given options:", activities)
+
+    if choice == 'User':
+        st.markdown('''<h5 style='text-align: left; color: #021659;'> Upload your resume, and get smart recommendations</h5>''',
+                    unsafe_allow_html=True)
+        pdf_file = st.file_uploader("Choose your Resume", type=["pdf"])
+        if pdf_file is not None:
+            with st.spinner('Uploading your Resume...'):
+                time.sleep(4)
+            save_image_path = './Uploaded_Resumes/'+pdf_file.name
+            with open(save_image_path, "wb") as f:
+                f.write(pdf_file.getbuffer())
+            show_pdf(save_image_path)
+            resume_data = ResumeParser(save_image_path).get_extracted_data()
+            if resume_data:
+                ## Get the whole resume data
+                resume_text = pdf_reader(save_image_path)
+
+                st.header("**Resume Analysis**")
+                st.success("Hello "+ resume_data['name'])
+                st.subheader("**Your Basic info**")
+                try:
+                    st.text('Name: '+resume_data['name'])
+                    st.text('Email: ' + resume_data['email'])
+                    st.text('Contact: ' + resume_data['mobile_number'])
+                    st.text('Resume pages: '+str(resume_data['no_of_pages']))
+                except:
+                    pass
+                cand_level = ''
+                if resume_data['no_of_pages'] == 1:
+                    cand_level = "Fresher"
+                    st.markdown( '''<h4 style='text-align: left; color: #d73b5c;'>You are at Fresher level!</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] == 2:
+                    cand_level = "Intermediate"
+                    st.markdown('''<h4 style='text-align: left; color: #1ed760;'>You are at intermediate level!</h4>''',unsafe_allow_html=True)
+                elif resume_data['no_of_pages'] >=3:
+                    cand_level = "Experienced"
+                    st.markdown('''<h4 style='text-align: left; color: #fba171;'>You are at experience level!''',unsafe_allow_html=True)
+
+
+run()
--- courses.py
+++ courses.py
@@ -52,12 +52,3 @@ uiux_course = [['Google UX Design Professional Certificate','https://www.courser
                ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
                ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
 
-resume_videos = ['https://youtu.be/3agP4x8LYFM','https://youtu.be/fS_t3yS8v5s',
-                 'https://youtu.be/aArb68OBFPg','https://youtu.be/h-NuvOeWWh0',
-                 'https://youtu.be/BdQniERyw8I','https://youtu.be/Tt08KmFfIYQ',
-                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/bhwEsfXS6y8']
-
-interview_videos = ['https://youtu.be/Tt08KmFfIYQ','https://youtu.be/KukmClH1KoA',
-                    'https://youtu.be/7_aAicmPB3A','https://youtu.be/1mHjMNZZvFo',
-                    'https://youtu.be/WfdtKbAJOmE','https://youtu.be/wFbU185CvDU'
-                    'https://youtu.be/wFbU185CvDU','https://youtu.be/TZ3C_syg9Ow']
